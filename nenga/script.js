e5ffb9fb417d5b9ac52c2250f33d87cbe9c9ac64
DOYSOUND-cmd <me1b16doi@gmail.com>
2025-10-19 22:46:30 +0900

    Delete nenga directory

--- nenga/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('card-container').addEventListener('click', () => {
-        document.getElementById('card').classList.toggle('flipped');
-    });
-});
